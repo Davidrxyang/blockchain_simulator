7a757a12392360fb802526a33b0aa0d55d726e4d
davidrxy <davidrxyang@hotmail.com>
2024-03-29 02:27:12 -0400

    genesis

--- blockchain.go
+++ blockchain.go
@@ -21,3 +21,13 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
+func NewGenesisBlock() *Block {
+	return NewBlock(0, "Genesis Block", "")
+}
+
+func NewBlock(index int, data, prevHash string) *Block {
+	block := &Block{Index: index, Timestamp: time.Now().String(), Data: data, PrevHash: prevHash}
+	block.Hash = block.CalculateHash()
+	block.MineBlock(difficulty)
+	return block
+}
